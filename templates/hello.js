dc0b23efcb852dd7b7bcb94c48116c4ac1ed72c1
kirinnee <kirinnee97@gmail.com>
2025-05-06 10:41:55 +0800

    fix: missing close braces

--- templates/hello.js
+++ templates/hello.js
@@ -5,15 +5,15 @@ function hello() {
 function goodbye() {
     // var___= if (language == "english") {___
         console.log("Goodbye, world!");
-    // var___= else if (language == "spanish") {___
+    // var___= } else if (language == "spanish") {___
         console.log("Adios, mundo!");
-    // var___= else if (language == "french") {___
+    // var___= } else if (language == "french") {___
         console.log("Au revoir, monde!");
-    // var___= else if (language == "german") {___
+    // var___= } else if (language == "german") {___
         console.log("Auf Wiedersehen, Welt!");
-    // var___= else if (language == "italian") {___
+    // var___= } else if (language == "italian") {___
         console.log("Ciao, mondo!");
-    // var___= else {___
+    // var___= } else {___
         console.log("Unknown language");
     // var___= }___
 }
